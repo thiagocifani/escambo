096dc57bf4e5c01bfb653b45694bb0e51cb14684
thiagocifani <cifani.thiago@gmail.com>
2014-04-25 16:49:23 -0300

    adds precision and scale to decimal columns

--- db/migrate/20140425193650_create_escambo_order_items.rb
+++ db/migrate/20140425193650_create_escambo_order_items.rb
@@ -2,7 +2,7 @@ class CreateEscamboOrderItems < ActiveRecord::Migration
   def change
     create_table :escambo_order_items do |t|
       t.references :product, index: true
-      t.decimal :price
+      t.decimal :price, precision: 10, scale: 2
       t.integer :quantity
       t.references :order, index: true
 
